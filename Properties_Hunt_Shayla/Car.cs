9f79bc11949bee34b38d0b37d53cbe9dc346755e
Shayla <Shaylahunt834@gmail.com>
2025-04-10 14:16:30 -0500

    Car

---
+++ Properties_Hunt_Shayla/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Hunt_Shayla
+{
+    internal class Car
+    {
+        // Private field to store the model of thhe Car
+        private string model;
+
+        // The getter and setter for the Model property
+        public string Model { get { return model; } set { model = value; } }
+
+        // A property used to store the make of the Car
+        public string Make { get; set; }
+
+    }
+}
--- Properties_Hunt_Shayla/Program.cs
+++ Properties_Hunt_Shayla/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the Car class and set its properties
+            Car car = new Car();
+            car.Make = "Toyota";
+            car.Model = "Carmy";
+
+            // Displays the properties of the car
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            // Creates another instance of the Car class and sets its properties
+            Car car2 = new Car();
+            car2.Make = "Honda";
+            car2.Model = "Civic";
+
+            // Displays the properties of the second car
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            // Creates a third instance of the Car class without setting properties
+            Car car3 = new Car();
         }
     }
 }
